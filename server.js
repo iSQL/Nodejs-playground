74ad6feee681c1cd052c893fe48b11e44f8342fe
iSQL <maremilojkovic@gmail.com>
2024-02-03 08:19:43 +0100

    - Changed API listening ports

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
